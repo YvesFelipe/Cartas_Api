4c4458db4ffd5d51060b9a7923d9fed62f2a0a2d
YvesFelipe <144679567+YvesFelipe@users.noreply.github.com>
2023-09-11 08:13:18 -0300

    Delete meu_app_api directory

--- meu_app_api/schemas/__init__.py
+++
@@ -1,5 +0,0 @@
-#from schemas.comentario import ComentarioSchema
-from schemas.carta import CartasSchema, CartasBuscaSchema, CartaViewSchema,  \
-                            ListagemCartasSchema, CartaDelSchema, apresenta_cartas, \
-                            apresenta_carta, apresenta_cartas
-from schemas.error import ErrorSchema
